1dc5aeaa4263603dd07d9c9a4292ac07ba88e8ff
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-12-30 14:31:16 +0900

    ひとまず簡単に既存のライブラリを持ってきた

---
+++ app/src/main/java/kobayashi/taku/com/heartratescan/HeartRateScanCamera.java
@@ -0,0 +1,128 @@
+package kobayashi.taku.com.heartratescan;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.hardware.Camera;
+
+import java.util.ArrayList;
+
+/**
+ * Created by akizuki on 2015/07/05.
+ */
+public class HeartRateScanCamera {
+    private Activity mActivity;
+    private Camera mCamera;
+    private Camera.Size mPreviewSize;
+    private HeartScanCallback mCallback;
+    //指を当てている間は赤がほとんどをしめるため、指が当たっているかどうかの判定に使う
+    private int redLightCounter = 0;
+    // 明るい部分を数える
+    private int lightFieldCount = 0;
+    // 指が当たっている時、直前の明るい部分を記録して比較するのに使う
+    private int prevSampling = 0;
+    // 明るい部分が前回との差分により増減していくことで、1ループで1ビートとカウントできそう
+    private int beatCounter = 0;
+    //前回とのビートの時でBPがなんとなく出そう
+    private long prevBeatSpan = 0;
+    private int mSumLoopBeatCount;
+    private ArrayList<Integer> bpmList;
+
+    public HeartRateScanCamera(Activity act){
+        mActivity = act;
+        bpmList = new ArrayList<Integer>();
+    }
+
+    public void setScanCallback(HeartScanCallback callback){
+        mCallback = callback;
+    }
+
+    public void scanStart(){
+        mCamera = Camera.open();
+        Camera.Parameters cp = mCamera.getParameters();
+
+        mCamera.setDisplayOrientation(Util.getCameraDisplayOrientation(mActivity, 0));
+        mPreviewSize = cp.getPreviewSize();
+        cp.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        mCamera.setParameters(cp);
+
+        mCamera.setPreviewCallback(new Camera.PreviewCallback() {
+            @Override
+            public void onPreviewFrame(byte[] data, Camera camera) {
+                lightFieldCount = 0;
+                redLightCounter = 0;
+                int[] rgb = Util.decodeYUV420SP(data, mPreviewSize.width, mPreviewSize.height, new Util.DecodeFinishListener() {
+                    @Override
+                    public int onDecode(int pixel, int wIndex, int hIndex) {
+                        int A = Color.alpha(pixel);
+                        int R = Color.red(pixel);
+                        int G = Color.green(pixel);
+                        int B = Color.blue(pixel);
+                        int grayscale = (int) (0.299 * R + 0.587 * G + 0.114 * B);
+                        if (R > 128) {
+                            ++redLightCounter;
+                        }
+                        if (grayscale > 128) {
+                            ++lightFieldCount;
+                        }
+                        return Color.argb(A, R, G, B);
+                    }
+                });
+                //指が当たっていない
+                if (redLightCounter < (mPreviewSize.width * mPreviewSize.height) * 0.9) {
+                    prevSampling = 0;
+                    beatCounter = 0;
+                    prevBeatSpan = System.currentTimeMillis();
+                    mSumLoopBeatCount = 0;
+                    bpmList.clear();
+                    return;
+                }
+                if (prevSampling == 0) {
+                    prevSampling = lightFieldCount;
+                    prevBeatSpan = System.currentTimeMillis();
+                } else {
+                    if (prevSampling < lightFieldCount) {
+                        ++beatCounter;
+                        if (beatCounter > 1) beatCounter = 1;
+                    } else {
+                        --beatCounter;
+                        if (beatCounter < -1) beatCounter = -1;
+                    }
+                    if (beatCounter == 0) {
+                        ++mSumLoopBeatCount;
+                        if(mSumLoopBeatCount % 2 == 0){
+                            long span = System.currentTimeMillis() - prevBeatSpan;
+                            int bpm = (int) ((float) 1 * 1000 * 60 / span);
+                            prevBeatSpan = System.currentTimeMillis();
+                            bpmList.add(bpm);
+                            int sum = 0;
+                            for(int i = 0;i < bpmList.size();++i) {
+                                sum += bpmList.get(i);
+                            }
+                            if(mCallback != null) mCallback.onBeat(sum / bpmList.size(), span);
+                        }
+
+                    }
+                    prevSampling = lightFieldCount;
+                }
+            }
+        });
+        mCamera.startPreview();
+    }
+
+    public void releaseCamera() {
+        if (mCamera != null){
+            Camera.Parameters cp = mCamera.getParameters();
+            cp.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+            mCamera.setParameters(cp);
+            mCamera.cancelAutoFocus();
+            mCamera.stopPreview();
+            mCamera.setPreviewCallback(null);
+            mCamera.release();
+            mCamera = null;
+        };
+    }
+
+    public interface HeartScanCallback{
+        public void onBeat(int bpm, long beatSpan);
+    }
+}
--- app/src/main/java/kobayashi/taku/com/heartratescan/MainActivity.java
+++ app/src/main/java/kobayashi/taku/com/heartratescan/MainActivity.java
@@ -2,12 +2,51 @@ package kobayashi.taku.com.heartratescan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private HeartRateScanCamera mHeartRateScanCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button heartScanButton = (Button) findViewById(R.id.HeartRateButton);
+        heartScanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mHeartRateScanCamera.scanStart();
+            }
+        });
+
+        TextView bpmLable = (TextView) findViewById(R.id.bpmLabelText);
+        bpmLable.setText(getString(R.string.bpmLabel, 0));
+
+        mHeartRateScanCamera = new HeartRateScanCamera(this);
+        mHeartRateScanCamera.setScanCallback(new HeartRateScanCamera.HeartScanCallback() {
+            @Override
+            public void onBeat(int bpm, long beatSpan) {
+                TextView bpmLable = (TextView) findViewById(R.id.bpmLabelText);
+                bpmLable.setText(getString(R.string.bpmLabel, bpm));
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mHeartRateScanCamera.releaseCamera();
     }
 }
